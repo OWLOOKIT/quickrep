ecaaac69dc532cfaa32bde97176d1370bdd2c259
Ruslan Voroshchuk <ruslan@owlookit.com>
2022-09-18 13:12:37 +0300

    add worksheet styling (wip)

--- src/Http/Controllers/TabularApiController.php
+++ src/Http/Controllers/TabularApiController.php
@@ -2,12 +2,15 @@
 
 namespace Owlookit\Quickrep\Http\Controllers;
 
+use Carbon\Carbon;
 use Owlookit\Quickrep\Http\Requests\QuickrepRequest;
 use Owlookit\Quickrep\Models\DatabaseCache;
 use Owlookit\Quickrep\Reports\Tabular\ReportGenerator;
 use Owlookit\Quickrep\Reports\Tabular\ReportSummaryGenerator;
 use DB;
 use PhpOffice\PhpSpreadsheet\Spreadsheet;
+use PhpOffice\PhpSpreadsheet\Style\Alignment;
+use PhpOffice\PhpSpreadsheet\Style\Fill;
 use PhpOffice\PhpSpreadsheet\Writer\Xlsx;
 use Symfony\Component\HttpFoundation\StreamedResponse;
 
@@ -45,30 +48,36 @@ class TabularApiController extends AbstractApiController
         $cache = new DatabaseCache( $report, $connectionName );
         $summaryGenerator = new ReportSummaryGenerator( $cache );
         $header = $summaryGenerator->runSummary();
-        $header = array_map( function( $element ) {
+        $lang=$report->getInput('lang') ?? config('app.locale');
+        $header = array_map( function( $element ) use ($lang) {
             // Replace spaces with '_' in the header
-            return preg_replace('/\s+/', '_', $element['title']);
+            $title = $element['title_I18n'][$lang] ? $element['title_I18n'][$lang] : $element['title'];
+            return preg_replace('/\s+/', '_', $title);
         }, $header );
         $reportGenerator = new ReportGenerator( $cache );
         $collection = $reportGenerator->getCollection();
 
+        $reportDescription = $report->GetReportDescriptionI18n()[$lang];
+
         // File name download should include MD5 from the contents of getCode #48
+        $reportName = (strlen($report->GetReportName()) > 150) ? substr($report->GetReportName(),0, 150) : $report->GetReportName();
+
         if ($report->getCode()) {
-            $filename = $report->GetReportName() . '-'.$report->getCode();
+            $filename = $reportName . '-'.$report->getCode();
         } else {
-            $filename = $report->GetReportName();
+            $filename = $reportName;
         }
 
         if ($fileType === 'csv') {
             $filename .= '.csv';
-            return $this->csvResponse($filename, $header, $collection);
+            return $this->csvResponse($filename, $reportDescription, $header, $collection);
         } else {
             $filename .= '.xlsx';
-            return $this->excelResponse($filename, $header, $collection);
+            return $this->excelResponse($filename, $reportDescription, $header, $collection);
         }
     }
 
-    protected function csvResponse($filename, $header, $collection)
+    protected function csvResponse($filename, $reportDescription, $header, $collection)
     {
         $response = new StreamedResponse( function() use ( $header, $collection ) {
             // Open output stream
@@ -98,15 +107,85 @@ class TabularApiController extends AbstractApiController
         return $response;
     }
 
-    protected function excelResponse($filename, $header, $collection)
+    protected function excelResponse($filename, $reportDescription, $header, $collection)
     {
-        $response = new StreamedResponse( function() use ($header, $collection) {
+        $response = new StreamedResponse( function() use ($filename, $reportDescription, $header, $collection) {
 
             $spreadsheet = new Spreadsheet();
+
+            $spreadsheet->getProperties()
+                ->setCreator(config("app.name" ))
+                ->setLastModifiedBy(config("app.name" ))
+                ->setTitle(preg_replace('/\\.[^.\\s]{3,4}$/', '', $filename))
+                ->setSubject(config("app.name" ) . " Report document")
+                ->setDescription(
+                    "Report document generated for Office 2007 XLSX."
+                )
+                ->setKeywords(config("app.name" ) . " report")
+                ->setCategory(config("app.name" ) . " report");
+
             $sheet = $spreadsheet->getActiveSheet();
+
+            $sheet->getSheetView()->setZoomScale(120);
+            $sheet->getTabColor()->setRGB('b2ebf2');
+
+            $sheet->getPageSetup()->setFitToWidth(1);
+            $sheet->getPageSetup()
+                ->setOrientation(\PhpOffice\PhpSpreadsheet\Worksheet\PageSetup::ORIENTATION_LANDSCAPE);
+            $sheet->getPageSetup()
+                ->setPaperSize(\PhpOffice\PhpSpreadsheet\Worksheet\PageSetup::PAPERSIZE_A4);
+
+            $drawing = new \PhpOffice\PhpSpreadsheet\Worksheet\HeaderFooterDrawing();
+            $drawing->setName('logo');
+            $drawing->setPath('./storage/assets/logo.png');
+            $drawing->setHeight(32);
+            $sheet->getHeaderFooter()->addImage($drawing, \PhpOffice\PhpSpreadsheet\Worksheet\HeaderFooter::IMAGE_HEADER_RIGHT);
+
+            $sheet->getHeaderFooter()
+                ->setOddHeader('&C&H&"Verdana,Trebuchet"&14'.$reportDescription.'&R&G');
+            $sheet->getHeaderFooter()
+                ->setOddFooter('&L&B' . $spreadsheet->getProperties()->getTitle() . ' | ' . Carbon::now() . '&RСтр. &P из &N');
+
+            $sheet->setTitle((strlen($spreadsheet->getProperties()->getTitle()) > 20) ? substr($spreadsheet->getProperties()->getTitle(),0,20).'...' : $spreadsheet->getProperties()->getTitle());
+
+            $styleArray = [
+                'font' => [
+                    'bold' => true,
+                ],
+                'alignment' => [
+                    'horizontal' => \PhpOffice\PhpSpreadsheet\Style\Alignment::HORIZONTAL_CENTER,
+                    'vertical' => \PhpOffice\PhpSpreadsheet\Style\Alignment::VERTICAL_CENTER,
+                ],
+                'borders' => [
+                    'top' => [
+                        'borderStyle' => \PhpOffice\PhpSpreadsheet\Style\Border::BORDER_THIN,
+                    ],
+                    'bottom' => [
+                        'borderStyle' => \PhpOffice\PhpSpreadsheet\Style\Border::BORDER_THIN,
+                    ],
+                    'left' => [
+                        'borderStyle' => \PhpOffice\PhpSpreadsheet\Style\Border::BORDER_THIN,
+                    ],
+                    'right' => [
+                        'borderStyle' => \PhpOffice\PhpSpreadsheet\Style\Border::BORDER_THIN,
+                    ],
+                ],
+                'fill' => [
+                    'fillType' => \PhpOffice\PhpSpreadsheet\Style\Fill::FILL_GRADIENT_LINEAR,
+                    'rotation' => 90,
+                    'startColor' => [
+                        'argb' => 'b2ebf2ff',
+                    ],
+                    'endColor' => [
+                        'argb' => '008fc1ff',
+                    ],
+                ],
+            ];
+            $sheet->getStyle([1, 1, sizeof($header), 1])->applyFromArray($styleArray);
 
             for ($i = 0, $l = sizeof($header); $i < $l; $i++) {
                 $sheet->setCellValueByColumnAndRow($i + 1, 1, $header[$i]);
+                $sheet->getColumnDimensionByColumn($i + 1)->setAutoSize(true);
             }
 
             for ($i = 0, $l = sizeof($collection); $i < $l; $i++) { // row $i
